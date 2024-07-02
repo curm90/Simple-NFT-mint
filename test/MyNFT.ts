fc81c0899dcd80ac27fab65a7f84359c6842f35e
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-02 17:04:30 +0100

    add minting NFT tests

--- test/MyNFT.ts
+++ test/MyNFT.ts
@@ -23,4 +23,28 @@ describe('MyMFT', function () {
       expect(await myNft.tokenCounter()).to.equal(0);
     });
   });
+
+  describe('Minting NFTs', async function () {
+    it('Should let owner mint NFTs', async function () {
+      const { owner, myNft } = await deployContract();
+      await myNft.connect(owner).createNFT();
+
+      expect(await myNft.ownerOf(0)).to.equal(owner.address);
+    });
+    it('Should increment the tokenCounter after NFT is minted', async function () {
+      const { owner, myNft } = await deployContract();
+
+      expect(await myNft.tokenCounter()).to.equal(0);
+      await myNft.connect(owner).createNFT();
+      await myNft.connect(owner).createNFT();
+      expect(await myNft.tokenCounter()).to.equal(2);
+    });
+    it('Should revert with custom error if non owner tries to mint NFT', async function () {
+      const { addr1, myNft } = await deployContract();
+
+      await expect(myNft.connect(addr1).createNFT())
+        .to.be.revertedWithCustomError(myNft, 'OwnableUnauthorizedAccount')
+        .withArgs(addr1.address);
+    });
+  });
 });
